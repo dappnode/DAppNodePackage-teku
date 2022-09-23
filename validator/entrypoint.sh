04613d9f60f8463386f0f47d57caeea3c13b0f05
Tropicar <zerozept@protonmail.com>
2022-09-23 12:15:19 +0200

    Check vars and exit command
    The user must write i want to exit my validators and the keystores in the keystores_voluntary_exit
    The format for this value is:
    0x898493....,0x84938493..

--- validator/entrypoint.sh
+++ validator/entrypoint.sh
@@ -5,6 +5,22 @@ NETWORK="mainnet"
 VALIDATOR_PORT=3500
 WEB3SIGNER_API="http://web3signer.web3signer.dappnode:9000"
 
+
+if [[ "$EXIT_VALIDATOR" == "I want to exit my validators" ]]; then
+    echo "Check connectivity with the web3signer"
+    WEB3SIGNER_STATUS=$(curl -s  http://web3signer.web3signer.dappnode:9000/healthcheck | jq '.status')
+    if [[ "$WEB3SIGNER_STATUS" == '"UP"' ]]; then
+    echo "Proceeds to do the voluntary exit of the next keystores:"
+    echo "$KEYSTORES_VOLUNTARY_EXIT"
+    echo yes | exec /opt/teku/bin/teku voluntary-exit --beacon-node-api-endpoint=http://beacon-chain.teku.dappnode:3500 \
+        --validators-external-signer-public-keys=$KEYSTORES_VOLUNTARY_EXIT \
+        --validators-external-signer-url=$WEB3SIGNER_API
+    else
+      echo "The web3signer is not running or the teku package has not access to the web3signer"
+    fi
+fi
+
+
 # Teku must start with the current env due to JAVA_HOME var
 exec /opt/teku/bin/teku --log-destination=CONSOLE \
   validator-client \
