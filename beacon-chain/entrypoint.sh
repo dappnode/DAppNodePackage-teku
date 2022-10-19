9234fa38446e2df5a888f56e1aa277afdd31ec60
Pablo <mendez4a@gmail.com>
2022-10-19 13:18:18 +0200

    Implement stakers UI support

--- beacon-chain/entrypoint.sh
+++ beacon-chain/entrypoint.sh
@@ -3,6 +3,38 @@
 # Concatenate EXTRA_OPTS string
 [[ -n $CHECKPOINT_SYNC_URL ]] && EXTRA_OPTS="--initial-state=$(echo $CHECKPOINT_SYNC_URL | sed 's:/*$::')/eth/v2/debug/beacon/states/finalized ${EXTRA_OPTS}"
 
+
+case $_DAPPNODE_GLOBAL_EXECUTION_CLIENT_MAINNET in
+"geth.dnp.dappnode.eth")
+    HTTP_ENGINE="http://geth.dappnode:8551"
+    ;;
+"nethermind.public.dappnode.eth")
+    HTTP_ENGINE="http://nethermind.public.dappnode:8551"
+    ;;
+"erigon.dnp.dappnode.eth")
+    HTTP_ENGINE="http://erigon.dappnode:8551"
+    ;;
+"besu.public.dappnode.eth")
+    HTTP_ENGINE="http://besu.public.dappnode:8551"
+    ;;
+*)
+    echo "Unknown value for _DAPPNODE_GLOBAL_EXECUTION_CLIENT_MAINNET: $_DAPPNODE_GLOBAL_EXECUTION_CLIENT_MAINNET"
+    HTTP_ENGINE=$_DAPPNODE_GLOBAL_EXECUTION_CLIENT_MAINNET
+    ;;
+esac
+
+# MEVBOOST: https://docs.teku.consensys.net/en/latest/HowTo/Builder-Network/
+if [ -n "$_DAPPNODE_GLOBAL_MEVBOOST_MAINNET" ] && [ "$_DAPPNODE_GLOBAL_MEVBOOST_MAINNET" == "true" ]; then
+    echo "MEVBOOST is enabled"
+    MEVBOOST_URL="http://mev-boost.mev-boost.dappnode:18550"
+    if curl --retry 5 --retry-delay 5 --retry-all-errors "${MEVBOOST_URL}"; then
+        EXTRA_OPTS="--builder-endpoint=${MEVBOOST_URL} ${EXTRA_OPTS}"
+    else
+        echo "MEVBOOST is enabled but ${MEVBOOST_URL} is not reachable"
+        curl -X POST -G 'http://my.dappnode/notification-send' --data-urlencode 'type=danger' --data-urlencode title="${MEVBOOST_URL} is not available" --data-urlencode 'body=Make sure the mevboost is available and running'
+    fi
+fi
+
 exec /opt/teku/bin/teku \
     --network=mainnet \
     --data-base-path=/opt/teku/data \
@@ -20,5 +52,4 @@ exec /opt/teku/bin/teku \
     --metrics-port 8008 \
     --metrics-host-allowlist "*" \
     --log-destination=CONSOLE \
-    --validators-proposer-default-fee-recipient="${DEFAULT_FEE_RECIPIENT}" \
     $EXTRA_OPTS
--- validator/entrypoint.sh
+++ validator/entrypoint.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
-CLIENT="teku"
 NETWORK="mainnet"
 VALIDATOR_PORT=3500
 WEB3SIGNER_API="http://web3signer.web3signer.dappnode:9000"
 
+# MEVBOOST: https://docs.teku.consensys.net/en/latest/HowTo/Builder-Network/
+if [ -n "$_DAPPNODE_GLOBAL_MEVBOOST_MAINNET" ] && [ "$_DAPPNODE_GLOBAL_MEVBOOST_MAINNET" == "true" ]; then
+  echo "MEVBOOST is enabled"
+  MEVBOOST_URL="http://mev-boost.mev-boost.dappnode:18550"
+  if curl --retry 5 --retry-delay 5 --retry-all-errors "${MEVBOOST_URL}"; then
+    EXTRA_OPTS="--validators-builder-registration-default-enabled ${EXTRA_OPTS}"
+  else
+    echo "MEVBOOST is enabled but ${MEVBOOST_URL} is not reachable"
+    curl -X POST -G 'http://my.dappnode/notification-send' --data-urlencode 'type=danger' --data-urlencode title="${MEVBOOST_URL} is not available" --data-urlencode 'body=Make sure the mevboost is available and running'
+  fi
+fi
 
-if [[ "$EXIT_VALIDATOR" == "I want to exit my validators" ]] && ! [ -z "$KEYSTORES_VOLUNTARY_EXIT" ]; then
-    echo "Check connectivity with the web3signer"
-    WEB3SIGNER_STATUS=$(curl -s  http://web3signer.web3signer.dappnode:9000/healthcheck | jq '.status')
-    if [[ "$WEB3SIGNER_STATUS" == '"UP"' ]]; then
+if [[ "$EXIT_VALIDATOR" == "I want to exit my validators" ]]; then
+  echo "Check connectivity with the web3signer"
+  WEB3SIGNER_STATUS=$(curl -s http://web3signer.web3signer.dappnode:9000/healthcheck | jq '.status')
+  if [[ "$WEB3SIGNER_STATUS" == '"UP"' ]]; then
     echo "Proceeds to do the voluntary exit of the next keystores:"
     echo "$KEYSTORES_VOLUNTARY_EXIT"
     echo yes | exec /opt/teku/bin/teku voluntary-exit --beacon-node-api-endpoint=http://beacon-chain.teku.dappnode:3500 \
-        --validators-external-signer-public-keys=$KEYSTORES_VOLUNTARY_EXIT \
-        --validators-external-signer-url=$WEB3SIGNER_API
-    else
-      echo "The web3signer-prater is not running or the teku package has not access to the web3signer"
-      echo "The env var KEYSTORES_VOLUNTARY_EXIT: $KEYSTORES_VOLUNTARY_EXIT has a empty value"
-    fi
+      --validators-external-signer-public-keys=$KEYSTORES_VOLUNTARY_EXIT \
+      --validators-external-signer-url=$WEB3SIGNER_API
+  else
+    echo "The web3signer is not running or the teku package has not access to the web3signer"
+  fi
 fi
 
 #Handle Graffiti Character Limit
@@ -45,6 +54,6 @@ exec /opt/teku/bin/teku --log-destination=CONSOLE \
   --validators-graffiti="${graffitiString}" \
   --validator-api-keystore-file=/cert/teku_client_keystore.p12 \
   --validator-api-keystore-password-file=/cert/teku_keystore_password.txt \
-  --validators-proposer-default-fee-recipient="${DEFAULT_FEE_RECIPIENT}" \
+  --validators-proposer-default-fee-recipient="${FEE_RECIPIENT_ADDRESS}" \
   --logging=ALL \
   ${EXTRA_OPTS}
